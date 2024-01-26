b742316721710a30e84c27af74ba7219451b6df1
user <user@172.30.1.27>
2024-01-26 10:44:24 +0900

    members

---
+++ jan08web/src/main/java/com/poseidon/admin/Members.java
@@ -0,0 +1,29 @@
+package com.poseidon.admin;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/admin/members")
+public class Members extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public Members() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		RequestDispatcher rd = request.getRequestDispatcher("/admin/members.jsp");
+		rd.forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+}
--- jan08web/src/main/webapp/admin/members.jsp
+++ jan08web/src/main/webapp/admin/members.jsp
@@ -9,10 +9,7 @@
 <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/xeicon@2.3.3/xeicon.min.css">
 </head>
 <body>
-	<!-- 2024-01-26 관리자 페이지 만들기 -->
-	<!-- 틀 -->
 	<div class="wrap">
-		<!-- menu -->
 		<div class="menu">
 			<nav>
 				<ul>
@@ -25,19 +22,12 @@
 				</ul>
 			</nav>
 		</div>
-		<!-- 본문내용 -->
 		<div class="main">
-			<!-- 이 페이지에 오는 모든 사람은 관리자인지 검사를 합니다.
-			관리자 경우 보여주고, 로그인 하지 않았거나 일반 사용자는 로그인 페이지로 이동시킵니다. -->
 			<article>
 			
-				<div class="info">
-					왼쪽 메뉴를 선택하세요.
-				</div>
+				
 				
 			</article>
-			
-			
 		</div>
 	</div>
 </body>
